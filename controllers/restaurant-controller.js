d967021b71ddb876f8aa853c26c0c91fa88f8bca
Howhow Chen <debby8698@gmail.com>
2022-12-01 00:39:46 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,24 @@
-module.exports = {
-  getRestaurants: (req, res, next) => {
-    res.render('restaurants')
+const { Restaurant, Category } = require('../models')
+
+const restaurantController = {
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category]
+      })
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      res.render('restaurants', {
+        restaurants: data
+      })
+    } catch (err) {
+      next(err)
+    }
   }
 }
+
+module.exports = restaurantController
