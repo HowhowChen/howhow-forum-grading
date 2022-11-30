a8f717b212b964a37d808b72928e7e579b4cf7bc
Howhow Chen <debby8698@gmail.com>
2022-12-01 00:02:14 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,11 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.findAll({ raw: true })
-      res.render('admin/categories', { categories })
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+      res.render('admin/categories', { categories, category })
     } catch (err) {
       next(err)
     }
@@ -17,6 +20,18 @@ const categoryController = {
       await Category.create({
         name
       })
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      const { id } = req.params
+      const category = await Category.findByPk(id)
+      if (!category) throw new Error("Category doesn't exist!")
+      await category.update({ name })
       res.redirect('/admin/categories')
     } catch (err) {
       next(err)
