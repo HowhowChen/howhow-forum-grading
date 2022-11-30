b8931304a48a74d24223d7b7cc18adfdc0a4928e
Howhow Chen <debby8698@gmail.com>
2022-12-01 01:05:10 +0800

    feat: add restaurant dashboard page show view counts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -23,12 +23,25 @@ const restaurantController = {
     try {
       const { id } = req.params
       const restaurant = await Restaurant.findByPk(id, {
+        include: [Category]
+      })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      await restaurant.increment('viewCounts', { by: 1 })
+      res.render('restaurant', { restaurant: restaurant.toJSON() })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getDashboard: async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const restaurant = await Restaurant.findByPk(id, {
         raw: true,
         nest: true,
         include: [Category]
       })
       if (!restaurant) throw new Error("Restaurant didn't exist!")
-      res.render('restaurant', { restaurant })
+      res.render('dashboard', { restaurant })
     } catch (err) {
       next(err)
     }
