d582ff520e81e962d0ec6c3034fa119f003b085d
Howhow Chen <debby8698@gmail.com>
2022-12-04 21:57:48 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -49,6 +49,7 @@ app.use(methodOverride('_method'))
 //  設立路由 讀取該路徑中的檔案
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
